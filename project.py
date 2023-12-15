2fef026187b3c25e39d7f4d6e3967943dd11098b
uraihan <umair.umer26@gmail.com>
2023-12-15 22:20:51 +0200

    final script to be submitted

--- project.py
+++ project.py
@@ -12,7 +12,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 
 
 def importFiles(files, label):
@@ -42,7 +42,7 @@ def importFiles(files, label):
 
     return dataset,labels
 
-def extractFeatures(dataset):
+def extractFeatures(dataset, model):
     features = []
     fs = 44000
 
@@ -63,12 +63,29 @@ def extractFeatures(dataset):
         #  frequency below which a certain percentage of the power spectrum is concentrated
         spectral_rolloff = librosa.feature.spectral_rolloff(y=np.asarray(audio), sr=fs)
         
-        combined_features_for_CNN = np.hstack([spectral_bandwidth, spectral_centroid])
-        combined_features_for_CNN2= np.hstack([zerocrossing_rate, spectral_rolloff])
-        combined_features2=np.vstack([combined_features_for_CNN, combined_features_for_CNN2])
+        if model == "KNN":
+            combined_features = np.hstack([np.mean(mfccs, axis=1), np.std(mfccs, axis=1),
+                                       np.mean(spectral_bandwidth), np.std(spectral_bandwidth),
+                                       np.mean(spectral_centroid), np.std(spectral_centroid),
+                                       np.mean(spectral_contrast), np.std(spectral_contrast),
+                                       np.mean(zerocrossing_rate), np.std(zerocrossing_rate),
+                                       np.mean(spectral_rolloff), np.std(spectral_rolloff)])
+            
+            # mfccs = np.reshape(mfccs, (1,-1))
+            # spectral_contrast = np.reshape(spectral_contrast, (1,-1))
+            # combined_features = np.hstack([spectral_bandwidth, spectral_centroid, zerocrossing_rate, spectral_rolloff, mfccs, spectral_contrast])
+            features.append(combined_features)
         
-        features.append(combined_features2)
+        elif model == "CNN":
+            combined_features_for_CNN = np.hstack([spectral_bandwidth, spectral_centroid])
+            combined_features_for_CNN2= np.hstack([zerocrossing_rate, spectral_rolloff])
+            combined_features = np.vstack([combined_features_for_CNN,combined_features_for_CNN2])
+            
+            features.append(combined_features)
+        else:
+            raise Exception("model is not recognizable.")
 
+    features = np.array(features)
     return features
 
 
@@ -84,6 +101,7 @@ car_test_dataset, car_test_label = importFiles(carTest, 0)
 tram_dataset, tram_label = importFiles(tramTrain, 1)
 tram_test_dataset, tram_test_label = importFiles(tramTest, 1)
 
+# combine tram and cars dataset
 dataset_train = np.concatenate([tram_dataset, car_dataset], axis=0)
 labels_train = np.concatenate([tram_label, car_label], axis=0)
 
@@ -91,15 +109,13 @@ dataset_test = np.concatenate([tram_test_dataset,car_test_dataset], axis=0)
 labels_test = np.concatenate([tram_test_label, car_test_label], axis=0)
 
 # extract features from the dataset
-features_train = extractFeatures(dataset_train)
-features_test = extractFeatures(dataset_test)
+features_train = extractFeatures(dataset_train, "KNN")
+features_test = extractFeatures(dataset_test, "KNN")
 
-features_train = np.asarray(features_train).reshape((len(features_train), -1))
-features_test = np.asarray(features_test).reshape((len(features_test), -1))
+features_train = features_train.reshape((features_train.shape[0], -1))
+features_test = features_test.reshape((features_test.shape[0], -1))
 
-## K-Nearest Neighbour Classifier
-# change is needed here: 
-# X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+## K-Nearest Neighbour Classifier, with all features
 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(features_train, labels_train)
@@ -110,42 +126,48 @@ precision_knn = precision_score(labels_test, y_pred)
 recall_knn = recall_score(labels_test, y_pred)
 accuracy_knn = accuracy_score(labels_test, y_pred)
 
+## Convolutional Neural Network
 
-## CNN Classifier
-## Currently not working due to shape not agreeing
-# input_shape = (2, 432, 1)
+# extract features from the dataset
+features_train2 = extractFeatures(dataset_train, "CNN")
+features_test2 = extractFeatures(dataset_test, "CNN")
 
-# model = Sequential()
-# model.add(Conv2D(4, kernel_size=(2,2), activation='relu', input_shape=input_shape))
-# #model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-# model.add(Flatten())
-# model.add(Dense(16, activation='relu'))
-# model.add(Dense(8, activation='relu'))
-# model.add(Dense(1, activation='sigmoid'))
-# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+input_shape = (2, 432, 1)
 
-# model.summary()
+model = Sequential()
+model.add(Conv2D(4, kernel_size=(2,2), activation='relu', input_shape=input_shape))
+#model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Flatten())
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# history = model.fit(x=features_train, y=labels_train,batch_size=5, epochs=10, validation_split=0.2, shuffle=True)
+history = model.fit(x=features_train2,y=labels_train,batch_size=5,epochs=10,validation_split=0.2,shuffle=True)
 
-# output = out1 = model.predict(features_test)
+output = model.predict(features_test2)
+predictions = [1 if x > 0.5 else 0 for x in output]
 
-# # Setting predictions threshold
-# predictions = [1 if x > 0.5 else 0 for x in output]
+accuracy_cnn = accuracy_score(labels_test, predictions)
+precision_cnn = precision_score(labels_test, predictions)
+recall_cnn = recall_score(labels_test, predictions)
 
-# # Precision-Recall-Accuracy score
-# accuracy_cnn = accuracy_score(labels_test, predictions)
-# precision_cnn = precision_score(labels_test, predictions)
-# recall_cnn = recall_score(labels_test, predictions)
 
 print("Nearest Neighbour:")
 print("Accuracy:", accuracy_knn)
 print("Precision:", precision_knn)
 print("Recall:", recall_knn)
 
-# print("\n")
+print("CNN:")
+print("Accuracy:", accuracy_cnn)
+print("Precision:", precision_cnn)
+print("Recall:", recall_cnn)
 
-# print("CNN:")
-# print("Accuracy:", accuracy_cnn)
-# print("Precision:", precision_cnn)
-# print("Recall:", recall_cnn)
+print("Testing with audio number 10 from test data (0 means car, 1 means tram)")
+print("Real label: ", labels_test[9])
+
+print("KNN Prediction")
+print("Prediction: ", y_pred[9])
+
+print("CNN Prediction")
+print("Prediction: ", output[9])
