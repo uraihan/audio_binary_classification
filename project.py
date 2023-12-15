cfd80307ff745b10f26ff11ece0f191383b1dca9
uraihan <umair.umer26@gmail.com>
2023-12-15 12:01:37 +0200

    changed how the features data stacked. having problem and performance regression with CNN

--- project.py
+++ project.py
@@ -8,23 +8,12 @@ from librosa.display import specshow
 import glob
 
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+# from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-
-
-carTrain = glob.glob("cars/train/*.wav")
-carTest = glob.glob("cars/test/*.wav")
-
-tramTrain = glob.glob("trams/train/*.wav")
-tramTest = glob.glob("trams/test/*.wav")
-
-dataset=[]
-labels = []
 
 def importFiles(files, label):
     dataset = []
@@ -48,39 +37,17 @@ def importFiles(files, label):
         # # Now use fixed_length_data as your adjusted data
         data = fixed_length_data
 
-
-
-
         labels.append(label)
         dataset.append(data)
 
     return dataset,labels
 
-car_dataset, car_label  = importFiles(carTrain, 0)
-car_test_dataset, car_test_label = importFiles(carTest, 0)
-tram_dataset, tram_label = importFiles(tramTrain, 1)
-tram_test_dataset, tram_test_label = importFiles(tramTest, 1)
+def extractFeatures(dataset):
+    features = []
+    fs = 44000
 
-dataset = np.concatenate([tram_dataset, car_dataset], axis=0)
-labels = np.concatenate([tram_label, car_label], axis=0)
-
-dataset_test = np.concatenate([tram_test_dataset,car_test_dataset], axis=0)
-labels_test = np.concatenate([tram_test_label, car_test_label], axis=0)
-
-
-fs = 44000
-features=[]
-
-fs = 44000
-f2=[]
-
-def extractFeatures(dataset,model):
-    features=[]
-    features2=[]
     for audio in dataset:
         mfccs = librosa.feature.mfcc(y=np.asarray(audio), sr=fs, n_mfcc=40)
-        #features.append(mfccs)
-    
         # spectral spread
         spectral_bandwidth = librosa.feature.spectral_bandwidth(y=np.asarray(audio), sr=fs)
 
@@ -96,81 +63,89 @@ def extractFeatures(dataset,model):
         #  frequency below which a certain percentage of the power spectrum is concentrated
         spectral_rolloff = librosa.feature.spectral_rolloff(y=np.asarray(audio), sr=fs)
         
-        combined_features = np.hstack([np.mean(mfccs, axis=1), np.std(mfccs, axis=1),
-                                       np.mean(spectral_bandwidth), np.std(spectral_bandwidth),
-                                       np.mean(spectral_centroid), np.std(spectral_centroid),
-                                       np.mean(spectral_contrast), np.std(spectral_contrast),
-                                       np.mean(zerocrossing_rate), np.std(zerocrossing_rate),
-                                       np.mean(spectral_rolloff), np.std(spectral_rolloff)])
-        
         combined_features_for_CNN = np.hstack([spectral_bandwidth, spectral_centroid])
         combined_features_for_CNN2= np.hstack([zerocrossing_rate, spectral_rolloff])
-        combined_features2=np.vstack([combined_features_for_CNN,combined_features_for_CNN2])
-
-        if model == "KNN":
-            features.append(combined_features)
-        else:
-            features.append(combined_features2)
+        combined_features2=np.vstack([combined_features_for_CNN, combined_features_for_CNN2])
+        
+        features.append(combined_features2)
 
     return features
 
-features =extractFeatures(dataset,"KNN")
-features2 =extractFeatures(dataset,"CNN")
 
-features_test=extractFeatures(dataset_test,"KNN")
-features_test2=extractFeatures(dataset_test,"CNN")
+carTrain = glob.glob("cars/train/*")
+carTest = glob.glob("cars/test/*")
 
-features= np.asarray(features)
-features2= np.asarray(features2)
+tramTrain = glob.glob("trams/train/*")
+tramTest = glob.glob("trams/test/*")
 
-features_test= np.asarray(features_test)
-features_test2= np.asarray(features_test2)
+# import audio from files
+car_dataset, car_label  = importFiles(carTrain, 0)
+car_test_dataset, car_test_label = importFiles(carTest, 0)
+tram_dataset, tram_label = importFiles(tramTrain, 1)
+tram_test_dataset, tram_test_label = importFiles(tramTest, 1)
 
-# # all
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+dataset_train = np.concatenate([tram_dataset, car_dataset], axis=0)
+labels_train = np.concatenate([tram_label, car_label], axis=0)
 
+dataset_test = np.concatenate([tram_test_dataset,car_test_dataset], axis=0)
+labels_test = np.concatenate([tram_test_label, car_test_label], axis=0)
+
+# extract features from the dataset
+features_train = extractFeatures(dataset_train)
+features_test = extractFeatures(dataset_test)
+
+features_train = np.asarray(features_train).reshape((len(features_train), -1))
+features_test = np.asarray(features_test).reshape((len(features_test), -1))
+
+## K-Nearest Neighbour Classifier
 # change is needed here: 
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
 knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
+knn.fit(features_train, labels_train)
 
-y_pred = knn.predict(X_test)
+y_pred = knn.predict(features_test)
 
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-accuracy = accuracy_score(y_test, y_pred)
+precision_knn = precision_score(labels_test, y_pred)
+recall_knn = recall_score(labels_test, y_pred)
+accuracy_knn = accuracy_score(labels_test, y_pred)
 
 
+## CNN Classifier
+## Currently not working due to shape not agreeing
+# input_shape = (2, 432, 1)
 
-input_shape = (2, 432, 1)
+# model = Sequential()
+# model.add(Conv2D(4, kernel_size=(2,2), activation='relu', input_shape=input_shape))
+# #model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+# model.add(Flatten())
+# model.add(Dense(16, activation='relu'))
+# model.add(Dense(8, activation='relu'))
+# model.add(Dense(1, activation='sigmoid'))
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model = Sequential()
-model.add(Conv2D(4, kernel_size=(2,2), activation='relu', input_shape=input_shape))
-#model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(Flatten())
-model.add(Dense(16, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# model.summary()
 
-model.summary()
+# history = model.fit(x=features_train, y=labels_train,batch_size=5, epochs=10, validation_split=0.2, shuffle=True)
 
-history = model.fit(x=features2,y=labels,batch_size=5 ,epochs=10,validation_split=0.2,shuffle=True)
+# output = out1 = model.predict(features_test)
 
-output=out1= model.predict(features_test2)
+# # Setting predictions threshold
+# predictions = [1 if x > 0.5 else 0 for x in output]
 
-predictions = [1 if x > 0.5 else 0 for x in output]
-
-accuracy2 = accuracy_score(labels_test, predictions)
-precision2 = precision_score(labels_test, predictions)
-recall2 = recall_score(labels_test, predictions)
+# # Precision-Recall-Accuracy score
+# accuracy_cnn = accuracy_score(labels_test, predictions)
+# precision_cnn = precision_score(labels_test, predictions)
+# recall_cnn = recall_score(labels_test, predictions)
 
 print("Nearest Neighbour:")
-print("Precision:", precision)
-print("Recall:", recall)
+print("Accuracy:", accuracy_knn)
+print("Precision:", precision_knn)
+print("Recall:", recall_knn)
 
-print("CNN:")
-print("Accuracy:", accuracy2)
-print("Precision:", precision2)
-print("Recall:", recall2)
+# print("\n")
+
+# print("CNN:")
+# print("Accuracy:", accuracy_cnn)
+# print("Precision:", precision_cnn)
+# print("Recall:", recall_cnn)
